63b71c13de4e1e23ad721ee2462cd6f757cd2576
Mike Kentley <mike.kentley@gmail.com>
2022-01-03 22:24:28 -0800

    added, but did not connect, a FinishScene for Barry to go to after crossing finish line

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,21 @@
+using System.Net;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CrashDetector : MonoBehaviour
-{
+
+{[SerializeField] float loadDelay = 1.0f;
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
+    }
     // Start is called before the first frame update
 
     void OnTriggerEnter2D(Collider2D other) {
           if (other.tag == "Ground") {
-          UnityEngine.Debug.Log("You crashed!");
+              Invoke("ReloadScene", loadDelay);
         
       }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,12 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+[SerializeField] float loadDelay=2.0f;
     private const string Message = "You Finished!";
 
     void OnTriggerEnter2D(Collider2D other)
    {
      if (other.tag == "Player") {
-      SceneManager.LoadScene(0);
+       Invoke("reloadScene", loadDelay);
      }
    }
+
+  void reloadScene() {
+   SceneManager.LoadScene(0);
+
+
 }
+
+}
+
