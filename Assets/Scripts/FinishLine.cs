e5f0734737da80fe71e4940fdc5f4e597819942f
Mike Kentley <mike.kentley@gmail.com>
2022-01-03 22:52:06 -0800

    added finish stuff

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,18 +6,19 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
 [SerializeField] float loadDelay=2.0f;
+[SerializeField] string finishSceneName = "FinishScene";
+
     private const string Message = "You Finished!";
 
     void OnTriggerEnter2D(Collider2D other)
    {
      if (other.tag == "Player") {
-       Invoke("reloadScene", loadDelay);
+       Invoke("finishScene", loadDelay);
      }
    }
 
-  void reloadScene() {
-   SceneManager.LoadScene(0);
-
+  void finishScene() {
+   SceneManager.LoadScene(finishSceneName);
 
 }
 
